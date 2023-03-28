320cd98b1c6176c8f728231a43e4e7f881bce7cf
IgorKilipenko <igorkili@mail.ru>
2023-03-29 01:44:51 +0700

    solve sprint1 / step2 / less16 / app3 (Основы React / Жизненный цикл компонентов)

--- src/trainig/sprint1/step2/less16/app3/task.jsx
+++ src/trainig/sprint1/step2/less16/app3/task.jsx
@@ -8,7 +8,7 @@ const Film = ({ data }) => {
       }
       alt={data.nameRU}
     />
-  );
+  )
   return (
     <div>
       <div className={'img'}>{image}</div>
@@ -16,27 +16,47 @@ const Film = ({ data }) => {
       <p className={'description'}>{`${data.year}, ${data.country}`}</p>
       <p className={'description'}>{`${data.duration} мин.`}</p>
     </div>
-  );
-};
+  )
+}
 
 class App extends React.Component {
   state = {
     isLoading: false,
     hasError: false,
-    data: []
-  };
+    data: [],
+  }
 
   componentDidMount() {
-    // Ваш код здесь
+    this.getFilms()
   }
 
   getFilms = () => {
-    this.setState({ ...this.state, hasError: false, isLoading: true });
-    // Ваш код здесь
-  };
+    this.setState({ ...this.state, hasError: false, isLoading: true })
+
+    const endpoint = 'https://api.nomoreparties.co/beatfilm-movies'
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((responseData) => {
+        const error = responseData.error
+        this.setState({
+          ...this.state,
+          data: error ? [] : responseData,
+          hasError: error ? true : false,
+          isLoading: false,
+        })
+      })
+      .catch((error) => {
+        this.setState({
+          ...this.state,
+          data: [],
+          hasError: true,
+          isLoading: false,
+        })
+      })
+  }
 
   render() {
-    const { data, isLoading, hasError } = this.state;
+    const { data, isLoading, hasError } = this.state
     return (
       <div className="app grid">
         {isLoading && 'Загрузка...'}
@@ -46,6 +66,6 @@ class App extends React.Component {
           !!data.length &&
           data.map((film, index) => <Film key={index} data={film} />)}
       </div>
-    );
+    )
   }
 }
