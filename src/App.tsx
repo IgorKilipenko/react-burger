4aa0112b4b6e870f0882bfa0a6faf4d86c647375
IgorKilipenko <igorkili@mail.ru>
2023-03-30 00:03:45 +0700

    sedit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const Counter = () => {
+    const [state, setState] = React.useState({ seconds: 0 })
+
+    React.useEffect(() => {
+      const timer = setInterval(() => {
+        setState((prevState) => ({
+          ...prevState,
+          seconds: prevState.seconds + 1,
+        }))
+      }, 1000)
+      return () => {
+        clearInterval(timer)
+      }
+    }, [])
+    
+    return <div>{state.seconds} seconds</div>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +36,10 @@ function App() {
         >
           Learn React
         </a>
+        <Counter />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
