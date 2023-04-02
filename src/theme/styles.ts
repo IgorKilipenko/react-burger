9ac55f2e050a3628b355de6de0a11203397da0b1
IgorKilipenko <igorkili@mail.ru>
2023-04-02 08:25:54 +0700

    add app-header-bg var and clean

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -3,14 +3,8 @@ import { mode } from "@chakra-ui/theme-tools"
 
 export const semanticTokens = {
   colors: {
-    /*
-    "chakra-body-text": { _light: "gray.800", _dark: "white" },
-    "chakra-body-bg": { _light: "white", _dark: "#1c1c21" },
-    "chakra-border-color": { _light: "gray.200", _dark: "whiteAlpha.300" },
-    "chakra-subtle-bg": { _light: "gray.100", _dark: "gray.700" },
-    "chakra-placeholder-color": { _light: "gray.500", _dark: "whiteAlpha.400" },
-    */
     "link-inactive-color": { _light: "gray.800", _dark: "#8585AD" },
+    "app-header-bg": { _light: "white", _dark: "#1C1C21" },
   },
 }
 
@@ -20,9 +14,6 @@ export const styles: ChakraThemeType["styles"] = {
       fontFamily: "body",
       color: mode("gray.800", "white")(props),
       bg: mode("white", "#131316")(props),
-      /*transitionProperty: "background-color",
-      transitionDuration: "normal",
-      lineHeight: "base",*/
     },
     "*::placeholder": {
       color: "chakra-placeholder-color",
