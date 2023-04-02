6a83168f37b6debf9a0b8584f4f69164959174df
IgorKilipenko <igorkili@mail.ru>
2023-04-02 08:27:13 +0700

    wrap logo

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,13 +1,15 @@
 import { Logo as LargeLogo } from "@ya.praktikum/react-developer-burger-ui-components"
 import SmallLogo from "./small-logo"
 import { useBreakpointValue } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 
 const Logo = ({ type, breakpoint: bp }) => {
   const variant = useBreakpointValue({
     base: type ?? "small",
     [bp]: type ?? "large",
   })
-  return variant === "large" ? <LargeLogo /> : <SmallLogo />
+
+  return <Box> {variant === "large" ? <LargeLogo /> : <SmallLogo />} </Box>
 }
 
 export default Logo
