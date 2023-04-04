739af1484c590cc3378ac3cfabaaf71cb30c5908
IgorKilipenko <igorkili@mail.ru>
2023-04-04 14:08:43 +0700

    solve sprint1 / step7 / less5 / app3/3 (Продвинутый React / События и хендлеры. SyntheticEvent)

--- src/components/lootboxer/lootboxer.js
+++ src/components/lootboxer/lootboxer.js
@@ -1,49 +1,50 @@
-import React from 'react'
-import lootboxerStyles from './lootboxer.module.css'
+import React from "react"
+import lootboxerStyles from "./lootboxer.module.css"
 
-import lockImage from '../../images/lock.svg'
-import crumbsImage from '../../images/crumbs.png'
-import drinkImage from '../../images/drink.png'
-import monitorImage from '../../images/monitor.png'
-import mouseImage from '../../images/mouse.png'
-import videocardImage from '../../images/videocard.png'
-import wheelImage from '../../images/wheel.png'
+import lockImage from "../../images/lock.svg"
+import checkImage from "../../images/check.svg"
+import retryImage from "../../images/retry.svg"
+import crumbsImage from "../../images/crumbs.png"
+import drinkImage from "../../images/drink.png"
+import monitorImage from "../../images/monitor.png"
+import mouseImage from "../../images/mouse.png"
+import videocardImage from "../../images/videocard.png"
+import wheelImage from "../../images/wheel.png"
 
 const itemsData = [
   {
-    name: 'GeekForce RTX 9000',
-    description:
-      'Да-да, та самая видеокарта. Теперь вам не нужно продавать почку, чтобы поиграть в самые новые игры.',
+    name: "GeekForce RTX 9000",
+    description: "Да-да, та самая видеокарта. Теперь вам не нужно продавать почку, чтобы поиграть в самые новые игры.",
     image: videocardImage,
   },
   {
-    name: 'Aimbot MX420',
+    name: "Aimbot MX420",
     description:
-      'Топовая игровая мышь с гиперчувствительным сенсором и эргономичным дизайном. Главное, чтобы не забанили за допинг.',
+      "Топовая игровая мышь с гиперчувствительным сенсором и эргономичным дизайном. Главное, чтобы не забанили за допинг.",
     image: mouseImage,
   },
   {
-    name: 'SpeedyMonster',
+    name: "SpeedyMonster",
     description:
-      'Энергетический напиток, после которого можно играть в режиме нон-стоп всю неделю. Не злоупотребляйте.',
+      "Энергетический напиток, после которого можно играть в режиме нон-стоп всю неделю. Не злоупотребляйте.",
     image: drinkImage,
   },
   {
-    name: 'TurboRacer 001',
+    name: "TurboRacer 001",
     description:
-      'С этим рулём можно входить в любой поворот и не сбавлять скорость. Забирайте, если любите запах  жжёной резины.',
+      "С этим рулём можно входить в любой поворот и не сбавлять скорость. Забирайте, если любите запах  жжёной резины.",
     image: wheelImage,
   },
   {
-    name: 'Крошки из клавиатуры',
+    name: "Крошки из клавиатуры",
     description:
-      'Так бывает. Сегодня вы выигрываете, а завтра выковыриваете крошки из клавиатуры. Похоже, завтра уже наступило.',
+      "Так бывает. Сегодня вы выигрываете, а завтра выковыриваете крошки из клавиатуры. Похоже, завтра уже наступило.",
     image: crumbsImage,
   },
   {
-    name: 'CrystalClearHD 27HDX',
+    name: "CrystalClearHD 27HDX",
     description:
-      'Ультраширокий (Очень. Широкий. Очень.) дисплей, который позволит видеть всё (кроме зарплаты к концу месяца).',
+      "Ультраширокий (Очень. Широкий. Очень.) дисплей, который позволит видеть всё (кроме зарплаты к концу месяца).",
     image: monitorImage,
   },
 ]
@@ -67,19 +68,17 @@ class Lootboxer extends React.Component {
     }))
   }
 
+  handleTakeClick = () => {
+    this.setState({ ...this.state, currentItem: null })
+  }
+
   render() {
     return (
       <section className={lootboxerStyles.root}>
         <p className={lootboxerStyles.description}>
-          {this.state.currentItem && this.state.isHovered && (
-            <span>{this.state.currentItem.description}</span>
-          )}
+          {this.state.currentItem && this.state.isHovered && <span>{this.state.currentItem.description}</span>}
         </p>
-        <div
-          className={lootboxerStyles.card}
-          onMouseEnter={this.handleItemHover}
-          onMouseLeave={this.handleItemHover}
-        >
+        <div className={lootboxerStyles.card} onMouseEnter={this.handleItemHover} onMouseLeave={this.handleItemHover}>
           {!this.state.currentItem ? (
             <img src={lockImage} alt="Lock" />
           ) : (
@@ -87,22 +86,26 @@ class Lootboxer extends React.Component {
           )}
         </div>
 
-        <h2
-          className={lootboxerStyles.title}
-          onMouseEnter={this.handleItemHover}
-          onMouseLeave={this.handleItemHover}
-        >
+        <h2 className={lootboxerStyles.title} onMouseEnter={this.handleItemHover} onMouseLeave={this.handleItemHover}>
           {this.state.currentItem && this.state.currentItem.name}
         </h2>
 
         <div className={lootboxerStyles.controls}>
-          {!this.state.currentItem && (
-            <button
-              className={lootboxerStyles.button}
-              onClick={this.handleNewTryClick}
-            >
+          {!this.state.currentItem ? (
+            <button className={lootboxerStyles.button} onClick={this.handleNewTryClick}>
               Поехали
             </button>
+          ) : (
+            <>
+              <button className={lootboxerStyles.button} onClick={this.handleTakeClick}>
+                <img src={checkImage} alt="Check" />
+                Забираю
+              </button>
+              <button className={lootboxerStyles.button} onClick={this.handleNewTryClick}>
+                <img src={retryImage} alt="Retry" />
+                Ещё разок
+              </button>
+            </>
           )}
         </div>
       </section>
