0a75fd232a7de242be8aa5ab75e956df5508e4fc
IgorKilipenko <igorkili@mail.ru>
2023-04-07 18:52:54 +0700

    clean

--- src/theme/components/modal/modal-close-button-theme.ts
+++ src/theme/components/modal/modal-close-button-theme.ts
@@ -6,13 +6,8 @@ const helpers = createMultiStyleConfigHelpers(["container", "icon"])
 const baseStyle = defineStyle({
   [items.container]: {
     _hover: {
-      cursor: "pointer"
-    }
-  },
-  [items.icon]: {
-    _hover: {
-      bg: "red.300",
-      transform: "rotate(90deg)",
+      cursor: "pointer",
+      bg: "hovered-bg",
     },
   },
 })
