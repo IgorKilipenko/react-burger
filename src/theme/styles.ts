8d3a4eaece04be08d4c2063fcf347a2fe6ae50dc
IgorKilipenko <igorkili@mail.ru>
2023-04-07 15:27:03 +0700

    add text-primary-color and element-bg vars (not clean)

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -1,19 +1,21 @@
 import { type ChakraTheme as ChakraThemeType } from "@chakra-ui/react"
-import { mode } from "@chakra-ui/theme-tools"
+//import { mode } from "@chakra-ui/theme-tools"
 
 export const semanticTokens = {
   colors: {
     "link-inactive-color": { _light: "gray.800", _dark: "#8585AD" },
-    "app-header-bg": { _light: "white", _dark: "#1C1C21" },
+    "app-header-bg": { _light: "gray.200", _dark: "#1C1C21" },
     "body-bg": { _light: "white", _dark: "#131316" },
+    "element-bg": { _light: "gray.200", _dark: "#2f2f37" },
+    "text-primary-color": { _light: "gray.800", _dark: "white" },
   },
 }
 
 export const styles: ChakraThemeType["styles"] = {
-  global: (props) => ({
+  global: (/*props*/) => ({
     body: {
       fontFamily: "body",
-      color: mode("gray.800", "white")(props),
+      color: "text-primary-color", //mode("gray.800", "white")(props),
       bg: "body-bg",
     },
     "*::placeholder": {
@@ -22,6 +24,7 @@ export const styles: ChakraThemeType["styles"] = {
     "*, *::before, &::after": {
       borderColor: "chakra-border-color",
     },
+    //! Override yandex.p components styles
     ".constructor-element": {
       ".constructor-element__text": {
         marginRight: "20px",
@@ -34,7 +37,8 @@ export const styles: ChakraThemeType["styles"] = {
         WebkitLineClamp: "2",
         WebkitBoxOrient: "vertical",
         textOverflow: "ellipsis",
-        whiteSpace: "pre-wrap"
+        whiteSpace: "pre-wrap",
+        color: "text-primary-color",
       },
     },
   }),
