314dc11f4fad24c85d8524ca19e306a46be5de84
IgorKilipenko <igorkili@mail.ru>
2023-04-08 12:41:44 +0700

    add calcTotalPrice

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -1,3 +1,4 @@
+import React, { useCallback } from "react"
 import { Flex, Text } from "@chakra-ui/react"
 import { Button } from "@ya.praktikum/react-developer-burger-ui-components"
 import { CurrencyIcon } from "../common/icons"
@@ -17,13 +18,20 @@ const extractIngredientsByType = (ingredientsList) => {
 }
 
 const BurgerConstructor = (/*{ selectedIngredients }*/) => {
+  const calcTotalPrice = useCallback((ingredients) => ingredients.reduce((res, curr) => (res += curr.price), 0), [])
   const [bun, innerIngredients] = extractIngredientsByType(selectedIngredients)
+  const [totalPrice, setTotalPrice] = React.useState(0)
+
+  React.useEffect(() => {
+    setTotalPrice(calcTotalPrice(selectedIngredients))
+  }, [calcTotalPrice])
+
   return (
     <Flex direction={"column"} gap={10} pt={100} justify="space-between" align="center" h="100%" w="100%">
       <Burger bun={bun} ingredients={innerIngredients} />
       <Flex justify="end" align="center" w="100%" gap={10}>
         <Flex align="center" gap={2}>
-          <Text variant={"digitsMedium"}>5785</Text>
+          <Text variant={"digitsMedium"}>{totalPrice}</Text>
           <Icon as={CurrencyIcon} boxSize={9} />
         </Flex>
         <Button htmlType="button" type="primary" size="large" extraClass="ml-2">
