59a294ad17316f2a3bdd7d23ccc5ff4f1794e69f
IgorKilipenko <igorkili@mail.ru>
2023-04-10 02:16:28 +0700

    rm modal context

--- src/context/modal/index.ts
+++
@@ -1,4 +0,0 @@
-export { CreateModalContext, ModalContext, useModalContext } from "./modal-context"
-export { ModalContextProvider } from "./modal-context-provider"
-
-export type { ModalContextType } from "./modal-context"
