0b3b855a3fe99c78e4d2b27d78696c680c580b87
IgorKilipenko <igorkili@mail.ru>
2023-04-09 18:35:05 +0700

    fix select

--- src/components/burger-ingredients/utils.ts
+++ src/components/burger-ingredients/utils.ts
@@ -1,19 +1,19 @@
-import { CartListItemType } from "../../context/cart/cart-context"
+import { CartItemType } from "../../context/cart/cart-context"
 import { BurgerIngredientType, IngredientsTableView } from "../../data"
 
 export const selectIngredients = (
   ingredients: IngredientsTableView,
   bunId: string = "bun"
-): CartListItemType<BurgerIngredientType>[] => {
+): CartItemType<BurgerIngredientType>[] => {
   const randomIndex = (val: any[] | number) => Math.floor(Math.random() * (Array.isArray(val) ? val!.length : val))
 
   const bun = { item: ingredients[bunId][randomIndex(ingredients[bunId])], quantity: 1 }
   const innerIngredients = Object.keys(ingredients)
     .filter((key) => key !== bunId)
-    .reduce<CartListItemType<BurgerIngredientType>[]>((res, key) => {
+    .reduce<CartItemType<BurgerIngredientType>[]>((res, key) => {
       res.push(
         ...ingredients[key]
-          .slice(Math.floor(randomIndex(ingredients[key])))
+          .slice(0, randomIndex(Math.min(ingredients[key].length, 10)))
           .map((ingredient) => ({ item: ingredient, quantity: randomIndex(4) }))
       )
       return res
