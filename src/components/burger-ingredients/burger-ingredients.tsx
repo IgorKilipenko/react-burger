b675e073874827050a74d706dfc748f825540ea1
IgorKilipenko <igorkili@mail.ru>
2023-04-09 23:43:16 +0700

    move modal ingredient detail to root

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -4,9 +4,11 @@ import { Text } from "@chakra-ui/react"
 import { capitalizeFirstLetter } from "../../utils/string-processing"
 import { IngredientsTabPanel } from "./ingredients-tab-panel"
 import { CategorySection } from "./category-section"
-import { CategoryBase, IngredientsTableView } from "../../data"
+import { BurgerIngredientType, CategoryBase, IngredientsTableView } from "../../data"
 import { selectIngredients } from "./utils"
 import { useCartContext } from "../../context/cart"
+import { Modal } from "../modal"
+import { headerText, IngredientDetail } from "../ingredient-detail"
 
 export interface BurgerIngredientsProps {
   categories: CategoryBase[]
@@ -15,17 +17,21 @@ export interface BurgerIngredientsProps {
 }
 
 const BurgerIngredients = ({ categories, activeCategoryId, ingredients: ingredientsTable }: BurgerIngredientsProps) => {
-  const {addProductToCart} = useCartContext()
+  const { addProductToCart } = useCartContext()
   const categoriesRefs = React.useRef<({ ref: HTMLElement | null | undefined } & (typeof categories)[number])[]>(
     categories.map((c) => ({ ref: null, ...c }))
   )
   const [currentTabId, setCurrentTabId] = React.useState(activeCategoryId)
   const ratioRef = React.useRef({ categoryId: activeCategoryId, ratio: 1 })
+  const [modalOpen, setModalOpen] = React.useState(false)
+  const modalIngredientRef = React.useRef<BurgerIngredientType | null>(null)
 
   React.useEffect(() => {
     const selectedIngredients = selectIngredients(ingredientsTable)
-    selectedIngredients.forEach(x => {addProductToCart(x)})
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    selectedIngredients.forEach((x) => {
+      addProductToCart(x)
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   React.useEffect(() => {
@@ -53,24 +59,41 @@ const BurgerIngredients = ({ categories, activeCategoryId, ingredients: ingredie
     }
   }
 
+  const handleIngredientClick = (ingredient: BurgerIngredientType) => {
+    modalIngredientRef.current = ingredient
+    setModalOpen(true)
+  }
+
   return (
-    <Flex ref={null} direction="column">
-      <Text variant={"mainLarge"} pt={10} pb={5}>
-        {capitalizeFirstLetter("соберите бургер")}
-      </Text>
-      <IngredientsTabPanel items={categories} onTabClick={handleChangeActiveTab} activeTabId={currentTabId} />
-      <Flex direction="column" overflowY="auto" className="custom-scroll" mt={10} gap={10}>
-        {categories.map((category, i) => (
-          <CategorySection
-            key={`category-${category.id}-${i}`}
-            ref={(el) => (categoriesRefs.current[i].ref = el)}
-            category={category}
-            ingredients={ingredientsTable[category.id]}
-            onCategoryInView={handleCategoryInView}
-          />
-        ))}
+    <>
+      <Flex ref={null} direction="column">
+        <Text variant={"mainLarge"} pt={10} pb={5}>
+          {capitalizeFirstLetter("соберите бургер")}
+        </Text>
+        <IngredientsTabPanel items={categories} onTabClick={handleChangeActiveTab} activeTabId={currentTabId} />
+        <Flex direction="column" overflowY="auto" className="custom-scroll" mt={10} gap={10}>
+          {categories.map((category, i) => (
+            <CategorySection
+              key={`category-${category.id}-${i}`}
+              ref={(el) => (categoriesRefs.current[i].ref = el)}
+              category={category}
+              ingredients={ingredientsTable[category.id]}
+              onCategoryInView={handleCategoryInView}
+              onIngredientClick={handleIngredientClick}
+            />
+          ))}
+        </Flex>
       </Flex>
-    </Flex>
+      <Modal
+        headerText={headerText}
+        isOpen={modalOpen}
+        onCloseClick={() => {
+          setModalOpen(false)
+        }}
+      >
+        {modalIngredientRef.current ? <IngredientDetail ingredient={modalIngredientRef!.current} /> : null}
+      </Modal>
+    </>
   )
 }
 
--- src/components/common/card/ingredient-card.tsx
+++ src/components/common/card/ingredient-card.tsx
@@ -3,50 +3,40 @@ import { Square, Flex, Image, Text } from "@chakra-ui/react"
 import { Link } from "../link"
 import { CurrencyIcon } from "../icons"
 import { Icon } from "../icon"
-import { Modal } from "../../modal"
 import { Counter } from "@ya.praktikum/react-developer-burger-ui-components"
 import { type BurgerIngredientType } from "../../../data"
-import { IngredientDetail, headerText } from "../../ingredient-detail"
 
 export interface IngredientCardProps {
   ingredient: BurgerIngredientType
   selectedCount?: number
+  onItemClick?: () => void
 }
 
-const IngredientCard = React.memo(({ ingredient, selectedCount = 0 }: IngredientCardProps) => {
-  const [modalOpen, setModalOpen] = React.useState(false)
+const IngredientCard = React.memo(({ ingredient, selectedCount = 0, onItemClick }: IngredientCardProps) => {
+  const handleItemClick = () => {
+    onItemClick && onItemClick()
+  }
 
   return (
-    <>
-      <Modal
-        headerText={headerText}
-        isOpen={modalOpen}
-        onCloseClick={() => {
-          setModalOpen(false)
-        }}
-      >
-        <IngredientDetail ingredient={ingredient}/>
-      </Modal>
-      <Link isActive={true} onClick={() => setModalOpen(true)}>
-        <Flex position="relative" direction="column" align="center" _hover={{ bg: "hovered-bg" }}>
-          <Image src={ingredient.image} h={120} ml={4} mr={4} mt={6} />
-          <Flex gap={2} pt={1} pb={1}>
-            <Text variant={"digitsDefault"} align="center">
-              {ingredient.price}
-            </Text>
-            <Icon as={CurrencyIcon} />
-          </Flex>
-          <Text variant={"mainDefault"} align="center" h={12}>
-            {ingredient.name}
+    <Link isActive={true} onClick={handleItemClick}>
+      <Flex position="relative" direction="column" align="center" _hover={{ bg: "hovered-bg" }}>
+        <Image src={ingredient.image} h={120} ml={4} mr={4} mt={6} />
+        <Flex gap={2} pt={1} pb={1}>
+          <Text variant={"digitsDefault"} align="center">
+            {ingredient.price}
           </Text>
-          {selectedCount > 0 && (
-            <Square position="absolute" size={8} top={0} right={0}>
-              <Counter count={selectedCount} size="default" />
-            </Square>
-          )}
+          <Icon as={CurrencyIcon} />
         </Flex>
-      </Link>
-    </>
+        <Text variant={"mainDefault"} align="center" h={12}>
+          {ingredient.name}
+        </Text>
+        {selectedCount > 0 && (
+          <Square position="absolute" size={8} top={0} right={0}>
+            <Counter count={selectedCount} size="default" />
+          </Square>
+        )}
+      </Flex>
+    </Link>
   )
 })
 
