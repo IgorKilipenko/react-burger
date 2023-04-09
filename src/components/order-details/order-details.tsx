48e2d4ada9ebb4c360cf6b46309a708051da8003
IgorKilipenko <igorkili@mail.ru>
2023-04-10 01:55:33 +0700

    complete order detail

--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -1,4 +1,5 @@
 import { Flex, Text } from "@chakra-ui/react"
+import { Icon, Icons } from "../common"
 
 export interface OrderDetailsProps {
   orderId?: string | number
@@ -8,12 +9,22 @@ export const headerText = "Детали ингредиента"
 
 export const OrderDetails = ({ orderId = "034536" }: OrderDetailsProps) => {
   return (
-    <Flex>
-      <Flex direction="column" align="center">
-        <Text variant="mainDefault" overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap">
-          {orderId}
-        </Text>
+    <Flex direction="column" align="center" mb={8}>
+      <Text variant="digitsLarge" overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap">
+        {orderId}
+      </Text>
+      <Text variant="mainMedium" overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap" mt={8}>
+        Идентификатор заказа
+      </Text>
+      <Flex mb={15} mt={15}>
+        <Icon as={Icons.OrderSuccessIcon} />
       </Flex>
+      <Text variant="mainDefault" overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap" mb={2}>
+        Ваш заказ начали готовить
+      </Text>
+      <Text variant="mainDefault" overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap" mb={30} color={"link-inactive-color"}>
+        Дождитесь готовности на орбитальной станции
+      </Text>
     </Flex>
   )
 }
