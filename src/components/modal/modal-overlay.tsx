c963e6d8ccd7c89db79f43c0134fabb1a155b92e
IgorKilipenko <igorkili@mail.ru>
2023-04-10 14:55:40 +0700

    fix handleMouseDown for ModalOverlay

--- src/components/modal/modal-overlay.tsx
+++ src/components/modal/modal-overlay.tsx
@@ -1,4 +1,5 @@
 import { Flex } from "@chakra-ui/react"
+import React from "react"
 
 export interface ModalOverlayProps {
   children?: React.ReactNode
@@ -6,11 +7,16 @@ export interface ModalOverlayProps {
 }
 
 export const ModalOverlay = ({ children, onClick }: ModalOverlayProps) => {
+  const clickTargetRef = React.useRef<HTMLDivElement>(null)
+
   const handleMouseDown: React.MouseEventHandler<HTMLDivElement> = (e) => {
-    onClick && e.button === 0 && onClick(e)
+    if (e.target === clickTargetRef.current && e.button === 0) {
+      e.stopPropagation()
+      onClick && onClick(e)
+    }
   }
   return (
-    <Flex bg="overlay-bg" justify="center" align="center" grow={1} onMouseDown={handleMouseDown}>
+    <Flex ref={clickTargetRef} bg="overlay-bg" justify="center" align="center" grow={1} onMouseDown={handleMouseDown}>
       {children}
     </Flex>
   )
