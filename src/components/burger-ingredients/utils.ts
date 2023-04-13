176bd3a5905dadce50d8c180c9c7b4dba38fb3e9
IgorKilipenko <igorkili@mail.ru>
2023-04-13 21:58:19 +0700

    edit for select without bun

--- src/components/burger-ingredients/utils.ts
+++ src/components/burger-ingredients/utils.ts
@@ -7,7 +7,7 @@ export const selectIngredients = (
 ): CartItemType<BurgerIngredientType>[] => {
   const randomIndex = (val: any[] | number) => Math.floor(Math.random() * (Array.isArray(val) ? val!.length : val))
 
-  const bun = { item: ingredients[bunId][randomIndex(ingredients[bunId])], quantity: 1 }
+  const bun = ingredients[bunId] ? { item: ingredients[bunId][randomIndex(ingredients[bunId])], quantity: 1 } : null
   const innerIngredients = Object.keys(ingredients)
     .filter((key) => key !== bunId)
     .reduce<CartItemType<BurgerIngredientType>[]>((res, key) => {
@@ -18,5 +18,5 @@ export const selectIngredients = (
       )
       return res
     }, [])
-  return [bun, ...innerIngredients]
+  return [...(bun ? [bun] : []), ...innerIngredients]
 }
