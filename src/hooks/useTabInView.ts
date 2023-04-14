481838a19c2f0a083cbfc33f25f00e8d06898564
IgorKilipenko <igorkili@mail.ru>
2023-04-15 03:45:31 +0700

    fix useTabInView next iteration

--- src/hooks/useTabInView.ts
+++ src/hooks/useTabInView.ts
@@ -7,56 +7,76 @@ export interface TabItemBase {
 }
 
 export function useTabInView(tabs: TabItemBase[], lockScrollTimeout = 1000) {
+
+  /// For InView items observe (set from outside)
   const [state, setState] = React.useState<{ itemIdInView: ItemIdType | null; ratio: number }>({
     itemIdInView: tabs[0]?.id,
     ratio: 1,
   })
 
+  /// Calculated active tab (when ratio of item is maximum)
   const [currentTabId, setCurrentTabId] = React.useState<ItemIdType | null>(state.itemIdInView)
-  const ratioRef = React.useRef({ categoryId: state.itemIdInView, ratio: state.ratio })
-  const scrollRef = React.useRef<ItemIdType | null>(null)
 
+  /// For observe when max in view ratio changed for item
+  const primaryTabRef = React.useRef({ categoryId: state.itemIdInView, ratio: state.ratio })
+
+  /// For disable change active tab when force scrolling
+  const forceScrollToRef = React.useRef<ItemIdType | null>(null)  
+
+  /// Set active tab (set when onTabClick for example)
   const setCurrentTabIdForce = React.useCallback(
     (id: ItemIdType) => {
-      scrollRef.current = id
+      forceScrollToRef.current = id
       setCurrentTabId(id)
       setTimeout(() => {
-        if (scrollRef.current === id) {
-          scrollRef.current = null
+        if (forceScrollToRef.current === id) {
+          forceScrollToRef.current = null
         }
       }, lockScrollTimeout)
     },
     [lockScrollTimeout]
   )
-
-  const setInViewState = React.useCallback<typeof setState>((state) => {
-    setState((prevState) => {
-      const inViewItem = typeof state === "function" ? state(prevState) : state
-      return inViewItem
-    })
-  }, [])
-
+  
+  /// Update active tab if not force scroll
   const updateCurrentTab = React.useCallback((categoryId: ItemIdType) => {
     setCurrentTabId((prevState) => {
-      if (scrollRef.current && scrollRef.current === categoryId) {
-        scrollRef.current = null
+      if (forceScrollToRef.current && forceScrollToRef.current === categoryId) {
+        forceScrollToRef.current = null
       }
-      return scrollRef.current ? prevState : categoryId
+      return forceScrollToRef.current ? prevState : categoryId
     })
   }, [])
 
-  React.useEffect(() => {
-    if (ratioRef.current.categoryId !== state.itemIdInView && state.ratio > ratioRef.current.ratio) {
-      ratioRef.current = {
-        ...ratioRef.current,
-        categoryId: state.itemIdInView,
-        ratio: state.ratio,
+  /// Calculate active tab when scrolling
+  const calculateActiveTab = React.useCallback(
+    (prevState: typeof state) => {
+      if (
+        primaryTabRef.current.categoryId !== prevState.itemIdInView &&
+        prevState.ratio > primaryTabRef.current.ratio
+      ) {
+        primaryTabRef.current = {
+          categoryId: prevState.itemIdInView,
+          ratio: prevState.ratio,
+        }
+      } else if (primaryTabRef.current.categoryId === prevState.itemIdInView) {
+        primaryTabRef.current = { ...primaryTabRef.current, ratio: prevState.ratio ?? 0 }
       }
-    } else {
-      ratioRef.current = { ...ratioRef.current, ratio: state.ratio ?? 0 }
-    }
-    ratioRef.current.categoryId && updateCurrentTab(ratioRef.current.categoryId)
-  }, [state.itemIdInView, state.ratio, updateCurrentTab])
+      primaryTabRef.current.categoryId && updateCurrentTab(primaryTabRef.current.categoryId)
+    },
+    [updateCurrentTab]
+  )
 
-  return { currentTabId, ratio: ratioRef.current.ratio, setInViewState, setCurrentTabIdForce } as const
+  /// Set from outside itemInView state
+  const setInViewState = React.useCallback<typeof setState>(
+    (state) => {
+      setState((prevState) => {
+        const inViewItem = typeof state === "function" ? state(prevState) : state
+        calculateActiveTab(prevState)
+        return inViewItem
+      })
+    },
+    [calculateActiveTab]
+  )
+
+  return { currentTabId, ratio: primaryTabRef.current.ratio, setInViewState, setCurrentTabIdForce } as const
 }
