6aba7beb75f8e3eb8ffe5d8fb9f38059162b030d
IgorKilipenko <igorkili@mail.ru>
2023-04-17 02:12:24 +0700

    solve sprint2 / step5 / less9 / app1/2 (Библиотека Redux / Работа с формами в Redux)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,5 @@
 import React, { useMemo } from "react"
+import { useSelector } from "react-redux"
 import styles from "./app.module.css"
 import { Title } from "../../ui/title/title"
 import { Cart } from "../cart"
@@ -6,17 +7,22 @@ import { Delivery } from "../delivery"
 import { Checkout } from "../checkout"
 import { Recommend } from "../cart/recommend"
 import { TotalPrice } from "../common/total-price"
-import { useSelector } from "react-redux"
 
 const title = { cart: "Корзина", delivery: "Доставка", checkout: "Подтверждение заказа" }
 
 function App() {
-  const step = "cart"
+  const step = useSelector((store) => store.step)
 
   const content = useMemo(() => {
     switch (step) {
       case "cart": {
         return <Cart />
+      }
+      case "delivery": {
+        return <Delivery />
+      }
+      case "checkout": {
+        return <Checkout />
       }
       default: {
         return <Cart />
