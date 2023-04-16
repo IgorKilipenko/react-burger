7a3922d9bb411c983be5cff017b5c84a0787fd3a
IgorKilipenko <igorkili@mail.ru>
2023-04-17 01:17:26 +0700

    solve sprint2 / step5 / less8 / app3/5 (Библиотека Redux / Сайд-эффекты в Redux. Библиотека redux-thunk)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "./index.css"
-import App from "./components/app/app"
-import { createStore } from "redux"
+import React, { StrictMode } from "react"
+import { createStore, compose, applyMiddleware } from "redux"
 import { Provider } from "react-redux"
+import { createRoot } from "react-dom/client"
 import reportWebVitals from "./reportWebVitals"
+import thunk from "redux-thunk"
+import App from "./components/app/app"
+import "./index.css"
+
 import { rootReducer } from "./services/reducers"
 
-const store = createStore(rootReducer)
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose
 
-const root = ReactDOM.createRoot(document.querySelector("#root"))
+const enhancer = composeEnhancers(applyMiddleware(thunk))
+const store = createStore(rootReducer, enhancer)
+const rootElement = document.getElementById("root")
+const root = createRoot(rootElement)
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
