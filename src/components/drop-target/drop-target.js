65624237344aa5b000a77b4cb54ecac588075abb
IgorKilipenko <igorkili@mail.ru>
2023-04-19 20:29:27 +0700

    start solve sprint2 / chapter-6 / less-3 / app1/3: (Drag-n-drop в приложениях / Drag and Drop. Ближе к реальности)

---
+++ src/components/drop-target/drop-target.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+export default function DropTarget(props) {
+  const { puzzleElement } = props
+  return <li className="listItem">{puzzleElement.elementSrc && <img src={`./${puzzleElement.elementSrc}`} />}</li>
+}
