0e6a84ec8165186ce98346fa308a4db19d2e7338
IgorKilipenko <igorkili@mail.ru>
2023-04-20 01:03:40 +0700

    add isTouchEnabled to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { capitalizeFirstLetter } from "./string-processing"
 
 export const isBrowser = !!(typeof window !== "undefined" && window.document && window.document.createElement)
+export const isTouchEnabled = isBrowser && ("ontouchstart" in window || navigator.maxTouchPoints > 0)
