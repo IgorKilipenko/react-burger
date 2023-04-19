4e60238c79396800fddc0f96d42af800c41d132b
IgorKilipenko <igorkili@mail.ru>
2023-04-20 04:07:39 +0700

    start dev dnd for IngredientCard

--- src/components/burger-ingredients/ingredient-card.tsx
+++ src/components/burger-ingredients/ingredient-card.tsx
@@ -1,6 +1,7 @@
 import { useCallback, memo } from "react"
 import { type BurgerIngredientType } from "../../data"
 import { Card } from "../common/card/card"
+import { useDrag } from "react-dnd"
 
 export interface IngredientCardProps {
   ingredient: BurgerIngredientType
@@ -12,9 +13,14 @@ const IngredientCard = memo<IngredientCardProps>(({ ingredient, selectedCount = 
   const handleItemClick = useCallback(() => {
     onClick && onClick()
   }, [onClick])
+  const [, dragRef] = useDrag({
+    type: "ingredient",
+    item: { id: ingredient._id },
+  })
 
   return (
     <Card
+      ref={dragRef}
       name={ingredient.name}
       image={ingredient.image}
       price={ingredient.price}
