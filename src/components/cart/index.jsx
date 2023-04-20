3987a799e4e96278a494b7c4b7dfcb5a767d5879
IgorKilipenko <igorkili@mail.ru>
2023-04-21 02:33:55 +0700

    solved sprint-2 / chapter-6 / less-4 / app1/2: (Drag-n-drop в приложениях / Drag and Drop. Реальность. Библиотека React-DND)

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Tabs } from './tabs';
-import { ProductsContainer } from './products-container';
-import { PostponedContainer } from './postponed-container';
-import { useSelector } from 'react-redux';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { DndProvider } from 'react-dnd';
+import React from "react"
+import { Tabs } from "./tabs"
+import { ProductsContainer } from "./products-container"
+import { PostponedContainer } from "./postponed-container"
+import { useSelector } from "react-redux"
+import { HTML5Backend } from "react-dnd-html5-backend"
+import { DndProvider } from "react-dnd"
 
 export const Cart = () => {
-  const currentTab = useSelector(state => state.cart.currentTab);
+  const currentTab = useSelector((state) => state.cart.currentTab)
   return (
     <section>
-      <Tabs />
-      {currentTab === 'items' ? <ProductsContainer /> : <PostponedContainer />}
+      <DndProvider backend={HTML5Backend}>
+        <Tabs />
+        {currentTab === "items" ? <ProductsContainer /> : <PostponedContainer />}
+      </DndProvider>
     </section>
-  );
-};
+  )
+}
--- src/components/cart/product.jsx
+++ src/components/cart/product.jsx
@@ -1,42 +1,50 @@
-import React from 'react';
-import { AmountButton } from '../../ui/amount-button/amount-button';
-import { DeleteButton } from '../../ui/delete-button/delete-button';
-import styles from './product.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { DECREASE_ITEM, DELETE_ITEM, INCREASE_ITEM } from '../../services/actions/cart';
-import { priceFormat } from '../common/utils';
-import { useDrag } from 'react-dnd';
+import React from "react"
+import { AmountButton } from "../../ui/amount-button/amount-button"
+import { DeleteButton } from "../../ui/delete-button/delete-button"
+import styles from "./product.module.css"
+import { useDispatch, useSelector } from "react-redux"
+import { DECREASE_ITEM, DELETE_ITEM, INCREASE_ITEM } from "../../services/actions/cart"
+import { priceFormat } from "../common/utils"
+import { useDrag } from "react-dnd"
 
 export const Product = ({ src, id, text, qty, price }) => {
-  const dispatch = useDispatch();
-  const discount = useSelector(state => state.cart.promoDiscount);
-  const discountedPrice = ((price - price * (discount / 100)) * qty).toFixed(0);
+  const dispatch = useDispatch()
+  const discount = useSelector((state) => state.cart.promoDiscount)
+  const discountedPrice = ((price - price * (discount / 100)) * qty).toFixed(0)
   const onDelete = () => {
     dispatch({
       type: DELETE_ITEM,
-      id
-    });
-  };
+      id,
+    })
+  }
   const decrease = () => {
     if (qty === 1) {
-      onDelete();
+      onDelete()
     } else {
       dispatch({
         type: DECREASE_ITEM,
-        id
-      });
+        id,
+      })
     }
-  };
+  }
   const increase = () => {
     dispatch({
       type: INCREASE_ITEM,
-      id
-    });
-  };
+      id,
+    })
+  }
+
+  const [{ opacity }, dragRef] = useDrag({
+    type: "items",
+    item: { id },
+    collect: (monitor) => ({
+      opacity: monitor.isDragging() ? 0.5 : 1,
+    }),
+  })
 
   return (
-    <div className={`${styles.product}`}>
-      <div className={styles.productBox}>
+    <div className={`${styles.product}`} style={{ opacity }}>
+      <div ref={dragRef} className={styles.productBox}>
         <img className={styles.img} src={src} alt="фото товара." />
         <p className={styles.text}>{text}</p>
       </div>
@@ -46,12 +54,10 @@ export const Product = ({ src, id, text, qty, price }) => {
         <AmountButton onClick={increase}>+</AmountButton>
       </div>
       <div className={styles.price}>
-        <p className={`${styles.price} ${discount && styles.exPrice}`}>
-          {priceFormat(price * qty)}
-        </p>
+        <p className={`${styles.price} ${discount && styles.exPrice}`}>{priceFormat(price * qty)}</p>
         {discount && <p className={styles.price}>{priceFormat(discountedPrice)}</p>}
       </div>
       <DeleteButton onDelete={onDelete} />
     </div>
-  );
-};
+  )
+}
