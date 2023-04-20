bcb300016e2941471baa43bb7886c0349e206277
IgorKilipenko <igorkili@mail.ru>
2023-04-21 01:29:27 +0700

    start dev drop logic in Burger component

--- src/components/burger-constructor/burger.tsx
+++ src/components/burger-constructor/burger.tsx
@@ -1,9 +1,10 @@
 import React, { useCallback } from "react"
 import { Flex } from "@chakra-ui/react"
 import { useCartContext, type CartItemType } from "../../context/cart/cart-context"
-import { BurgerIngredientType } from "../../data"
+import { BurgerIngredientType, DbIndexType } from "../../data"
 import { BurgerItem, ElementType, allowableTypes } from "./burger-item"
 import { uid } from "uid"
+import { useDrop } from "react-dnd"
 
 export interface BurgerProps {}
 
@@ -15,17 +16,30 @@ export const Burger = React.memo<BurgerProps>(() => {
     }, null)
     return { bun, ingredients: innerIngredients }
   }, [])
+  const handleDrop = React.useCallback((id: DbIndexType) => {
+    console.log("drop")
+  }, [])
 
   const { cart: selectedIngredients } = useCartContext()
   const { bun, ingredients } = extractIngredientsByType(selectedIngredients)
 
+  const [{ isHover }, dropTarget] = useDrop({
+    accept: "ingredient",
+    drop(itemId) {
+      handleDrop(itemId as DbIndexType)
+    },
+    collect: (monitor) => ({
+      isHover: monitor.isOver(),
+    }),
+  })
+
   return (
     <Flex
+      ref={dropTarget}
       direction="column"
       justify="start"
       align="stretch"
       gap={4}
-      maxH="100%"
       overflowY="auto"
       overflowX="hidden"
       className="custom-scroll"
