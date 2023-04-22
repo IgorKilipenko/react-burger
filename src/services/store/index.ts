d4d59406bb61350508724c3ffa2ebb61a49071bf
IgorKilipenko <igorkili@mail.ru>
2023-04-23 05:33:18 +0700

    reconfig configureAppStore

--- src/services/store/index.ts
+++ src/services/store/index.ts
@@ -1,9 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import { burgerReducer } from "../slices/burger-constructor/burger-slice"
 
-export const store = configureStore({
-  reducer: { burgerConstructor: burgerReducer },
+const rootReducer = combineReducers({
+  burgerConstructor: burgerReducer,
+  
 })
+
+interface Options {
+  forceDisableLogger?: boolean
+}
+
+export default function configureAppStore({ forceDisableLogger = true }: Options) {
+  const isDevelopmentMode = () => process.env.NODE_ENV === "development"
+  const middlewares: any[] = []
+  if (isDevelopmentMode() && !forceDisableLogger) {
+    const { logger } = require(`redux-logger`)
+
+    middlewares.push(logger)
+  }
+
+  const store = configureStore({
+    reducer: rootReducer,
+    devTools: isDevelopmentMode(),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(...middlewares),
+  })
+
+  if (isDevelopmentMode() && module.hot) {
+    module.hot.accept("./", () => store.replaceReducer(rootReducer))
+  }
+
+  return store
+}
+
+export const store = configureAppStore({ forceDisableLogger: true })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
