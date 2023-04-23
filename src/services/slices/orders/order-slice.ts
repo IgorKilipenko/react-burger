59d5aecd7bc3a355bd5b81ab4ead1c0aab215fca
IgorKilipenko <igorkili@mail.ru>
2023-04-23 12:12:57 +0700

    add orderList in orderSlice for next

--- src/services/slices/orders/order-slice.ts
+++ src/services/slices/orders/order-slice.ts
@@ -1,23 +1,25 @@
 import { createSlice, createAsyncThunk, SerializedError, PayloadAction } from "@reduxjs/toolkit"
 import { api } from "../../../data"
 
-export const createOrder = createAsyncThunk("order/createOrder", async (ingredients: string[], thunkApi) => {
+export const createOrder = createAsyncThunk("order/createOrder", async (ingredientsIds: string[], thunkApi) => {
   thunkApi.dispatch(clearOrder())
-  const { data, error } = await api.createOrder(ingredients)
+  const { data, error } = await api.createOrder(ingredientsIds)
   if (error || !data?.success) throw error ?? Error(data?.message ?? "Error.")
-  return { data, error }
+  return { data, orderList: [...ingredientsIds], error }
 })
 
-type Response = Awaited<ReturnType<typeof api.createOrder>>
+type Response = Awaited<ReturnType<(typeof api.createOrder)>> & {orderList?: string [] | null}
 
 interface OrderResponseState {
   order?: NonNullable<Response["data"]>["order"] | null
+  orderList?: NonNullable<Response["orderList"]> | null
   error?: SerializedError | null
   loading: boolean
 }
 
 const initialState: OrderResponseState = {
   order: null,
+  orderList: null,
   error: null,
   loading: false,
 }
@@ -33,10 +35,12 @@ const orderSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(createOrder.fulfilled, (state, { payload }: PayloadAction<Response>) => {
       state.order = payload.data?.order
+      state.orderList = payload?.orderList
       state.loading = false
     })
     builder.addCase(createOrder.rejected, (state, { error }) => {
       state.order = initialState.order
+      state.orderList = initialState.orderList
       state.error = error
       state.loading = false
     })
