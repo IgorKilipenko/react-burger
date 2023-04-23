f6a4dfdced17c5e12a313c06ca21184e36251275
IgorKilipenko <igorkili@mail.ru>
2023-04-24 06:43:13 +0700

    add type helpers

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -1,1 +1,7 @@
-export type Omitted<T, V> = { [K in keyof T]: T[K] extends V ? never : K }[keyof T]
+import { FlexProps } from "@chakra-ui/react"
+
+export type WithoutIntersect<T, V> = { [K in keyof T]: T[K] extends V ? never : K }[keyof T]
+
+export type SelectProps<T extends {}, K extends keyof T> = Omit<T, keyof Omit<T, K>>
+export type SelectFlexProps<K extends keyof FlexProps> = SelectProps<FlexProps, K>
+export type OmitFlexProps<K extends keyof FlexProps> = Omit<FlexProps, K>
