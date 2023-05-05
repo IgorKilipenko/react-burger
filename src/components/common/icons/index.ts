703b22b0a17946e223c72bb0a8abd7b55d864e74
IgorKilipenko <igorkili@mail.ru>
2023-05-05 15:33:39 +0700

    add EditIcon

--- src/components/common/icons/index.ts
+++ src/components/common/icons/index.ts
@@ -1,3 +1,4 @@
+import { Icon  as ChakraIcon, IconProps as ChakraIconProps } from "@chakra-ui/react"
 export { CloseIcon } from "./close-icon"
 export { ProfileIcon } from "./profile-icon"
 export { BurgerIcon } from "./burger-icon"
@@ -8,3 +9,7 @@ export { CheckMarkIcon } from "./check-mark-icon"
 export { OrderSuccessIcon } from "./order-success-icon"
 export { LockIcon } from "./lock-icon"
 export { DeleteIcon } from "./delete-icon"
+export { EditIcon } from "./edit-icon"
+
+export type IconType = typeof ChakraIcon
+export type IconProps = ChakraIconProps;
