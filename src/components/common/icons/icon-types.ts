d596387d56713358586b030a40fe1d90eee12f8d
IgorKilipenko <igorkili@mail.ru>
2023-05-05 15:54:05 +0700

    add ShowIcon & icon-types

---
+++ src/components/common/icons/icon-types.ts
@@ -0,0 +1,3 @@
+import { IconProps as ChakraIconProps } from "@chakra-ui/react"
+
+export interface IconProps extends ChakraIconProps {}
--- src/components/common/icons/index.ts
+++ src/components/common/icons/index.ts
@@ -1,4 +1,4 @@
-import { Icon  as ChakraIcon, IconProps as ChakraIconProps } from "@chakra-ui/react"
+export type { IconProps } from "./icon-types"
 export { CloseIcon } from "./close-icon"
 export { ProfileIcon } from "./profile-icon"
 export { BurgerIcon } from "./burger-icon"
@@ -10,6 +10,4 @@ export { OrderSuccessIcon } from "./order-success-icon"
 export { LockIcon } from "./lock-icon"
 export { DeleteIcon } from "./delete-icon"
 export { EditIcon } from "./edit-icon"
-
-export type IconType = typeof ChakraIcon
-export type IconProps = ChakraIconProps;
+export { ShowIcon } from "./show-icon"
