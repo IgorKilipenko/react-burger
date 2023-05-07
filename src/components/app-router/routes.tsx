d314f0c7199d8588524a65968c841df6fb7ff8ac
IgorKilipenko <igorkili@mail.ru>
2023-05-07 21:21:35 +0700

    start mig to ProtectedRoute

--- src/components/app-router/routes.tsx
+++ src/components/app-router/routes.tsx
@@ -1,19 +1,21 @@
 import { HomePage, NotFoundPage } from "../../pages"
 import { LoginPage } from "../../pages/login"
+import { ProfilePage } from "../../pages/profile"
 import { RegisterPage } from "../../pages/register"
 import App from "../app"
 import { IngredientDetail } from "../ingredient-details"
 import { Layout } from "../layouts"
+import { ProtectedRoute } from "./protected-route"
 
 export const routesInfo = {
   root: { path: "/", element: <App /> },
   layout: { element: <Layout /> },
   home: { path: "/", element: <HomePage /> },
-  login: { path: "/login", element: <LoginPage/> },
+  login: { path: "/login", element: <ProtectedRoute accessType="withoutAuth" element={<LoginPage/>} redirectPath={"/"}/> },
   register: { path: "/register", element: <RegisterPage/> },
   forgotPassword: { path: "/forgot-password", element: null },
   resetPassword: { path: "/reset-password", element: null },
-  profile: { path: "/profile", element: null },
+  get profile() {return { path: "/profile", element: <ProtectedRoute accessType="withAuth" element={<ProfilePage/>} redirectPath={this.login.path}/> }},
   ingredientItem: {
     id: "ingredientItem",
     paramName: "id",
