0c2f223172141caf1938e90b685f69d25eb9655b
IgorKilipenko <igorkili@mail.ru>
2023-05-07 23:07:08 +0700

    protect register in routes

--- src/components/app-router/routes.tsx
+++ src/components/app-router/routes.tsx
@@ -7,15 +7,28 @@ import { IngredientDetail } from "../ingredient-details"
 import { Layout } from "../layouts"
 import { ProtectedRoute } from "./protected-route"
 
+const rootPath = "/"
+
 export const routesInfo = {
-  root: { path: "/", element: <App /> },
+  root: { path: rootPath, element: <App /> },
   layout: { element: <Layout /> },
-  home: { path: "/", element: <HomePage /> },
-  login: { path: "/login", element: <ProtectedRoute accessType="withoutAuth" element={<LoginPage/>} redirectPath={"/"}/> },
-  register: { path: "/register", element: <RegisterPage/> },
+  home: { path: rootPath, element: <HomePage /> },
+  login: {
+    path: "/login",
+    element: <ProtectedRoute accessType="withoutAuth" element={<LoginPage />} redirectPath={rootPath} />,
+  },
+  register: {
+    path: "/register",
+    element: <ProtectedRoute accessType="withoutAuth" element={<RegisterPage />} redirectPath={rootPath} />,
+  },
   forgotPassword: { path: "/forgot-password", element: null },
   resetPassword: { path: "/reset-password", element: null },
-  get profile() {return { path: "/profile", element: <ProtectedRoute accessType="withAuth" element={<ProfilePage/>} redirectPath={this.login.path}/> }},
+  get profile() {
+    return {
+      path: "/profile",
+      element: <ProtectedRoute accessType="withAuth" element={<ProfilePage />} redirectPath={this.login.path} />,
+    }
+  },
   ingredientItem: {
     id: "ingredientItem",
     paramName: "id",
