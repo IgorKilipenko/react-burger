fd442af01d3301341b09713de55fec08ef6af382
IgorKilipenko <igorkili@mail.ru>
2023-05-08 01:02:17 +0700

    complete ProfilePage

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,30 +1,55 @@
 import React from "react"
-import { Form } from "../../components/common/form"
 import { Flex, Text } from "@chakra-ui/react"
-import { routesInfo } from "../../components/app-router"
+import { UserForm } from "../../components/user-form"
 import { useAppSelector } from "../../services/store"
 import { getAuthStore } from "../../services/slices/auth"
-import { useNavigate } from "react-router-dom"
+import { Link } from "../../components/common"
+import { routesInfo } from "../../components/app-router"
+import { useLocation } from "react-router-dom"
 
 export const ProfilePage = () => {
-  const navigate = useNavigate()
   const authState = useAppSelector(getAuthStore)
+  const location = useLocation()
+  const placeholders = React.useMemo(() => {
+    if (!authState.isAuthenticatedUser) return undefined
 
-  /*React.useEffect(() => {
-    authState.isAuthenticatedUser && navigate(routesInfo.home.path, { replace: true })
-  }, [authState.isAuthenticatedUser, navigate])*/
+    return {
+      name: authState.user.name,
+      email: authState.user.email,
+      password: Array(6)
+        .fill("*")
+        .reduce((acc, val) => acc + val),
+    }
+  }, [authState.isAuthenticatedUser, authState.user.email, authState.user.name])
+
+  const buildLink = React.useCallback(
+    ({ text, path }: { text: string; path: string }) => {
+      return (
+        <Flex w="320px" h="64px" align="center">
+          <Link to={path} value={path} isActive={location.pathname === path}>
+            <Text variant="mainMedium">{text}</Text>
+          </Link>
+        </Flex>
+      )
+    },
+    [location.pathname]
+  )
 
   return (
     <Flex align="center" justify="center" grow={1}>
-      <Form
-        method="post"
-        //onSubmit={handleSubmit}
-        //options={{ control: { isInvalid: !!authState.error && !hasChanged } }}
-      >
-        <Flex direction="column" align="center" gap={6} pb={20}>
-          <Text variant="mainMedium">Профиль</Text>
+      <Flex gap={15}>
+        <Flex direction="column">
+          <Flex direction="column" pb={20}>
+            {buildLink({ text: "Профиль", path: routesInfo.profile.path })}
+            {buildLink({ text: "История заказов", path: "#" })}
+            {buildLink({ text: "Выход", path: "#" })}
+          </Flex>
+          <Flex color="link-inactive-color">
+            <Text variant="mainDefault">В этом разделе вы можете изменить свои персональные данные</Text>
+          </Flex>
         </Flex>
-      </Form>
+        <UserForm withEditIcons={true} values={placeholders}></UserForm>
+      </Flex>
     </Flex>
   )
 }
