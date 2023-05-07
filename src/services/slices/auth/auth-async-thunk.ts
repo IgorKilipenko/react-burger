4c0478798258a13189648096631a405ced92e142
IgorKilipenko <igorkili@mail.ru>
2023-05-07 21:19:55 +0700

    edit error message for loginAsyncThunk

--- src/services/slices/auth/auth-async-thunk.ts
+++ src/services/slices/auth/auth-async-thunk.ts
@@ -7,11 +7,15 @@ export interface LoginResponse extends Omit<Awaited<ReturnType<typeof api.auth.l
   data: WithoutTokens<ApiLoginResponseType>
 }
 
-export const login = createAsyncThunk("auth/login", async (userData: WithPassword<Omit<UserDataType, "name">>)  => {
+export const login = createAsyncThunk("auth/login", async (userData: WithPassword<Omit<UserDataType, "name">>) => {
   const { data, error } = await api.auth.login(userData)
-  if (error || !data || !data.success) throw error ?? Error(data?.message ?? "Неизвестная ошибка авторизации")
+
+  if (error || !data || !data.success)
+    throw Error(
+      data?.message ?? (error?.message && error.message.length > 0 ? error.message : "Неизвестная ошибка авторизации")
+    )
   console.assert(data!.accessToken.length > 0 && data!.refreshToken.length > 0)
-  
+
   tokenManager.saveToken(data!.accessToken, "access")
   tokenManager.saveToken(data!.refreshToken, "refresh")
 
@@ -22,11 +26,8 @@ export interface RegisterResponse extends Omit<Awaited<ReturnType<typeof api.aut
   data: WithoutTokens<ApiRegisterResponseType>
 }
 
-export const register = createAsyncThunk(
-  "auth/register",
-  async (userData: WithPassword<UserDataType>) => {
-    const { data, error } = await api.auth.register(userData)
-    if (error || !data?.success) throw error ?? Error(data?.message ?? "Неизвестная ошибка регистрации")
-    return { data: data as WithoutTokens<ApiLoginResponseType>, error } as LoginResponse
-  }
-)
+export const register = createAsyncThunk("auth/register", async (userData: WithPassword<UserDataType>) => {
+  const { data, error } = await api.auth.register(userData)
+  if (error || !data?.success) throw error ?? Error(data?.message ?? "Неизвестная ошибка регистрации")
+  return { data: data as WithoutTokens<ApiLoginResponseType>, error } as LoginResponse
+})
