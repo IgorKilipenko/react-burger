af225ea2307284d556dddc5e6d82d01c77250962
IgorKilipenko <igorkili@mail.ru>
2023-05-08 06:37:01 +0700

    fix scaffold & set logout dispatch

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 import { Flex, Text } from "@chakra-ui/react"
 import { UserForm } from "../../components/user-form"
-import { useAppSelector } from "../../services/store"
-import { getAuthStore } from "../../services/slices/auth"
+import { useAppDispatch, useAppSelector } from "../../services/store"
+import { authActions, getAuthStore } from "../../services/slices/auth"
 import { Link } from "../../components/common"
 import { routesInfo } from "../../components/app-router"
 import { useLocation } from "react-router-dom"
@@ -10,6 +10,7 @@ import { useLocation } from "react-router-dom"
 export const ProfilePage = () => {
   const authState = useAppSelector(getAuthStore)
   const location = useLocation()
+  const dispatch = useAppDispatch()
   const placeholders = React.useMemo(() => {
     if (!authState.isAuthenticatedUser) return undefined
 
@@ -23,10 +24,10 @@ export const ProfilePage = () => {
   }, [authState.isAuthenticatedUser, authState.user.email, authState.user.name])
 
   const buildLink = React.useCallback(
-    ({ text, path }: { text: string; path: string }) => {
+    ({ text, path, onClick }: { text: string; onClick?: () => void; path?: string }) => {
       return (
-        <Flex w="320px" h="64px" align="center">
-          <Link to={path} value={path} isActive={location.pathname === path}>
+        <Flex w="100%" h="64px" align="center">
+          <Link to={path} onClick={onClick} value={path} isActive={location.pathname === path}>
             <Text variant="mainMedium">{text}</Text>
           </Link>
         </Flex>
@@ -36,13 +37,18 @@ export const ProfilePage = () => {
   )
 
   return (
-    <Flex align="center" justify="center" grow={1}>
-      <Flex gap={15}>
-        <Flex direction="column">
-          <Flex direction="column" pb={20}>
+    <Flex align="start" justify="center" grow={1}>
+      <Flex gap={15} mt="28">
+        <Flex direction="column" w="min-content">
+          <Flex direction="column" pb={30} w="320px">
             {buildLink({ text: "Профиль", path: routesInfo.profile.path })}
             {buildLink({ text: "История заказов", path: "#" })}
-            {buildLink({ text: "Выход", path: "#" })}
+            {buildLink({
+              text: "Выход",
+              onClick: () => {
+                dispatch(authActions.logout())
+              },
+            })}
           </Flex>
           <Flex color="link-inactive-color">
             <Text variant="mainDefault">В этом разделе вы можете изменить свои персональные данные</Text>
