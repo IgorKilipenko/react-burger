0b04b4d68ac9fb465e9d136bda379359d38390bc
IgorKilipenko <igorkili@mail.ru>
2023-05-10 05:18:05 +0700

    fix clearErrors in ProfileContainer

--- src/components/layouts/profile-container.tsx
+++ src/components/layouts/profile-container.tsx
@@ -2,24 +2,23 @@ import React from "react"
 import { Flex } from "@chakra-ui/react"
 import { Outlet, useLocation } from "react-router-dom"
 import { usePrevious } from "../../hooks"
-import { authActions } from "../../services/slices/auth"
-import { useAppDispatch } from "../../services/store"
+import { authActions, getAuthStore } from "../../services/slices/auth"
+import { useAppDispatch, useAppSelector } from "../../services/store"
 import { FlexOptions } from "../../utils/types"
 
 export interface ProfileContainerProps extends FlexOptions {}
 
 export const ProfileContainer = React.memo<ProfileContainerProps>((props) => {
   const dispatch = useAppDispatch()
+  const authState = useAppSelector(getAuthStore)
   const location = useLocation()
   const prevPathname = usePrevious<string>(location.pathname)
 
   React.useEffect(() => {
-    return () => {
-      if (prevPathname && prevPathname !== location.pathname) {
-        dispatch(authActions.clearErrors())
-      }
+    if (prevPathname && prevPathname !== location.pathname) {
+      authState.error && dispatch(authActions.clearErrors())
     }
-  }, [dispatch, location.pathname, prevPathname])
+  }, [authState.error, dispatch, location.pathname, prevPathname])
 
   return (
     <Flex direction="column" align="center" justify="center" grow={1} {...(props as FlexOptions)}>
