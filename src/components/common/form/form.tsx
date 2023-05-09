47709869e8219ab85908af63c8b300d5cde215ce
IgorKilipenko <igorkili@mail.ru>
2023-05-09 22:19:57 +0700

    edit flex options for form

--- src/components/common/form/form.tsx
+++ src/components/common/form/form.tsx
@@ -1,18 +1,12 @@
-import {
-  Flex,
-  FormControl,
-  type FormControlProps,
-  type HTMLChakraProps,
-} from "@chakra-ui/react"
+import { Flex, FormControl, type FormControlProps, type HTMLChakraProps } from "@chakra-ui/react"
 import { Form as RoutingForm, type FormProps as RoutingFormProps } from "react-router-dom"
 import { FlexOptions } from "../../../utils/types"
 
 export type FormControlOptions = Omit<FormControlProps, keyof HTMLChakraProps<"div">>
 
-export interface FormProps extends Omit<RoutingFormProps, "children"> {
+export interface FormProps extends Omit<RoutingFormProps, "children">, FlexOptions {
   children?: RoutingFormProps["children"]
   options?: {
-    container?: FlexOptions
     control?: FormControlOptions
   }
 }
@@ -21,7 +15,7 @@ export const Form: React.FC<FormProps> = ({ children, ...props }) => {
   const formControlOptions = props.options?.control ?? {}
   const formProps = props as RoutingFormProps
   return (
-    <Flex>
+    <Flex {...(props as FlexOptions)}>
       <FormControl as="div" {...formControlOptions}>
         <RoutingForm {...formProps}>{children}</RoutingForm>
       </FormControl>
