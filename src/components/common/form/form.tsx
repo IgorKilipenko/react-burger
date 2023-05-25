f12271dc4043f45a0754fe3bcd1663404bddc0f6
IgorKilipenko <igorkili@mail.ru>
2023-05-25 16:56:47 +0700

    mig to FlexContainer for form props

--- src/components/common/form/form.tsx
+++ src/components/common/form/form.tsx
@@ -1,10 +1,10 @@
 import { Flex, FormControl, type FormControlProps, type HTMLChakraProps } from "@chakra-ui/react"
 import { Form as RoutingForm, type FormProps as RoutingFormProps } from "react-router-dom"
-import { FlexOptions } from "../../../utils/types"
+import { FlexContainer } from "../../../utils/types"
 
 export type FormControlOptions = Omit<FormControlProps, keyof HTMLChakraProps<"div">>
 
-export interface FormProps extends Omit<RoutingFormProps, "children">, FlexOptions {
+export interface FormProps extends Omit<RoutingFormProps, "children">, FlexContainer {
   children?: RoutingFormProps["children"]
   options?: {
     control?: FormControlOptions
@@ -15,7 +15,7 @@ export const Form: React.FC<FormProps> = ({ children, ...props }) => {
   const formControlOptions = props.options?.control ?? {}
   const formProps = props as RoutingFormProps
   return (
-    <Flex {...(props as FlexOptions)}>
+    <Flex {...(props as FlexContainer)}>
       <FormControl as="div" {...formControlOptions}>
         <RoutingForm {...formProps}>{children}</RoutingForm>
       </FormControl>
